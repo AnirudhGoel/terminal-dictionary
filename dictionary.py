b202677be94651ffe210a3b03268e73f4bfbb7c8
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2016-12-12 23:32:58 +0530

    added case for empty execution of file

--- dictionary.py
+++ dictionary.py
@@ -10,23 +10,25 @@ if query_length > 1:
 	for x in range(1,query_length):
 		term = term + " " + sys.argv[x]
 
-term = term.strip()
+	term = term.strip()
 
-url = "http://api.duckduckgo.com/?q=" + term + "&format=json&pretty=1"
+	url = "http://api.duckduckgo.com/?q=" + term + "&format=json&pretty=1"
 
-r = requests.get(url)
-data = r.content
+	r = requests.get(url)
+	data = r.content
 
-json_data = json.loads(str(data))
+	json_data = json.loads(str(data))
 
-count = len(json_data["RelatedTopics"])
+	count = len(json_data["RelatedTopics"])
 
-if count == 0:
-	print "No meaning found for this term."
-elif count == 1:
-	print json_data["RelatedTopics"][0]["Text"]
+	if count == 0:
+		print "No meaning found for this term."
+	elif count == 1:
+		print json_data["RelatedTopics"][0]["Text"]
+	else:
+		count = 3 if count >= 3 else count
+		print "The term %s may refer to " %term
+		for x in xrange(0,count):
+			print str(x+1) + " : " + json_data["RelatedTopics"][x]["Text"]
 else:
-	count = 3 if count >= 3 else count
-	print "The term %s may refer to " %term
-	for x in xrange(0,count):
-		print str(x+1) + " : " + json_data["RelatedTopics"][x]["Text"]
+	print "Please enter a term to search for its meaning."
